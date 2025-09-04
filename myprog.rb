f3fa410920160ac9b96569e88b17460abbd46f02
adamrturman <adamrturman@gmail.com>
2025-09-04 15:09:58 -0500

    feat: fizzbuzz and contains duplicate

--- myprog.rb
+++ myprog.rb
@@ -1,5 +1,40 @@
 def say_hello_goodbye(name)
-	return "Hello #{name} and goodbye"
+	"Hello #{name.upcase} and goodbye"
 end
 
-puts(say_hello_goodbye("Adam"))
+def fizzbuzz(start, ending)
+	range = (start..ending)
+	range.each do |i|
+		if i % 15 == 0
+			puts "Fizzbuzz"
+		elsif i % 5 == 0
+			puts "Buzz"
+		elsif i % 3 == 0
+			puts "Fizz"
+		else
+			puts i
+		end
+	end
+end
+
+# fizzbuzz(1, 150)
+
+def contains_duplicate(nums)
+	answer = false
+	hash = {}
+
+	if nums.length > 0
+		nums.each do |i|
+			if hash[i]
+				answer = true
+			else
+				hash[i] = "blub"
+			end
+		end
+	end
+	 puts answer
+end
+
+contains_duplicate([1,2,3])
+contains_duplicate([1,2,3,3])
+contains_duplicate([])
