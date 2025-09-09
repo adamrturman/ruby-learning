e97141d3429e6a951130cfb8cd32d901d02463ed
adamrturman <adamrturman@gmail.com>
2025-09-09 11:41:54 -0500

    feat: blocks, argv, outputs

--- myprog.rb
+++ myprog.rb
@@ -27,6 +27,7 @@ def contains_duplicate(nums)
 		nums.each do |i|
 			if hash[i]
 				answer = true
+				break
 			else
 				hash[i] = "blub"
 			end
@@ -38,3 +39,61 @@ end
 contains_duplicate([1,2,3])
 contains_duplicate([1,2,3,3])
 contains_duplicate([])
+
+def two_sum(nums, target)
+	seen = {}
+
+	nums.each_with_index do |num, i|
+		diff = target - num
+		if seen[diff]
+			return [seen[diff], i]
+		else
+			seen[num] = i
+		end
+	end
+end
+
+def process_message(text)
+  print "Processing: '#{text}'"
+  
+  # The magic happens here - we yield to the block!
+  result = yield(text) if block_given?
+  
+  print "Result: '#{result}'"
+  result
+end
+
+process_message("hello world") { |b| b.upcase }
+# Processing: 'hello world'
+# Result: 'HELLO WORLD'
+
+process_message("Ruby is awesome") { |msg| msg.reverse }
+# Processing: 'Ruby is awesome'  
+# Result: 'emosewa si ybuR'
+
+process_message("secret message") do |msg|
+  # Multi-line blocks use do...end
+  msg.split('').map { |char| char == 'e' ? '3' : char }.join
+end
+# Processing: 'secret message'
+# Result: 's3cr3t m3ssag3'
+
+process_message("will not crash without a block")
+
+5.times { puts "^"}
+
+print "Loading"
+5.times do
+  sleep(0.5)
+  print "."
+end
+print " Done!\n"
+
+("a"..."t").each do |char|
+	print char
+end
+
+("a".."t").each { print _1 }
+
+puts "You gave me #{ARGV.size} #{ARGV.size == 1 ? 'argument' : 'arguments'}"
+p ARGV
