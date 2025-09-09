9ba4b6d69b350b52398e25cba33f92f8a2dc172c
adamrturman <adamrturman@gmail.com>
2025-09-09 13:53:17 -0500

    feat: more with classes

---
+++ fight.rb
@@ -0,0 +1,69 @@
+class Fighter
+  attr_accessor :health, :name
+
+  def initialize(name, health)
+    @name = name
+    @health = Float(health)
+  end
+end
+
+class Fight
+  def initialize(fighter_one, fighter_two)
+    @fighter_one = fighter_one
+    @fighter_two = fighter_two
+    @over = false
+  end
+
+  def to_s
+    "Current score #{@fighter_one.name} - #{@fighter_one.health} vs #{@fighter_two.name} - #{@fighter_two.health}"
+  end
+
+  def punch(fighter)
+    if @over 
+      puts "Fight is over"
+      return
+    end
+    reduce_health(fighter)
+    check_for_winner
+  end
+
+  private def reduce_health(fighter)
+    fighter.health -= 1
+  end
+
+  private def check_for_winner
+    if @fighter_one.health == 0
+      puts "#{@fighter_two.name} has won!"
+      @over = true
+      return @fighter_two
+    elsif @fighter_two.health == 0
+      puts "#{@fighter_one.name} has won!"
+      @over = true
+      return @fighter_one
+    end
+  end
+end
+
+rocky = Fighter.new("Rocky", 10)
+apollo = Fighter.new("Apollo", 10)
+
+fight = Fight.new(rocky, apollo)
+
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(apollo)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+fight.punch(rocky)
+
+
+puts fight
